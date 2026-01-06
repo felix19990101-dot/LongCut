cb972b31b2400177abd2b1c3290f56a5679e5143
felix19990101-dot <felix19990101@gmail.com>
2026-01-06 23:38:25 +0800

    Add files via upload

---
+++ 20260106195434/start-server.bat
@@ -0,0 +1,16 @@
+@echo off
+echo ======================================
+echo  YouTube 视频分析工具 - 启动本地服务器
+echo ======================================
+echo.
+echo 正在启动本地 HTTP 服务器...
+echo 请勿关闭此窗口！
+echo.
+echo 在浏览器中访问: http://localhost:8000
+echo 按 Ctrl+C 可以停止服务器
+echo.
+echo ======================================
+
+python -m http.server 8000
+
+pause
